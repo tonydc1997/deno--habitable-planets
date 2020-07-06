375776e0ba0b64c1500e394960556e6419e81791
Anthony Curtis <tonydc1997@gmail.com>
2020-07-06 12:48:01 -0400

    feat: Define Planet obj/interface
    - It's an array of strings where each property within arr is a string.

--- mod.ts
+++ mod.ts
@@ -2,6 +2,10 @@ import { join } from "https://deno.land/std/path/mod.ts";
 import { BufReader } from "https://deno.land/std/io/bufio.ts";
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 
+interface Planet {
+  [key: string]: string;
+}
+
 async function loadPlanetsData() {
   const path = join(".", "kepler_exoplanets_nasa.csv");
 
