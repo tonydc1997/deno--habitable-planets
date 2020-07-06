d530ea42da79f05d296085536568cac22bd757ed
Anthony Curtis <tonydc1997@gmail.com>
2020-07-06 12:12:02 -0400

    feat: Parse result in buffered fashion

--- mod.ts
+++ mod.ts
@@ -5,6 +5,11 @@ import { parse } from "https://deno.land/std/encoding/csv.ts";
 async function loadPlanetsData() {
   const path = join(".", "kepler_exoplanets_nasa.csv");
   const file = await Deno.open(path);
+  const bufReader = new BufReader();
+  const result = await parse(bufReader, {
+    header: true,
+    comment: "#",
+  });
 
   console.log(data);
 }
