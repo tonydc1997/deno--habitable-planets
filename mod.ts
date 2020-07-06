8484eed6318c76be13990a73422baa284c4b0d31
Anthony Curtis <tonydc1997@gmail.com>
2020-07-06 12:19:03 -0400

    chore: Adjust spacing for better comprehension

--- mod.ts
+++ mod.ts
@@ -4,14 +4,15 @@ import { parse } from "https://deno.land/std/encoding/csv.ts";
 
 async function loadPlanetsData() {
   const path = join(".", "kepler_exoplanets_nasa.csv");
+
   const file = await Deno.open(path);
   const bufReader = new BufReader(file);
   const result = await parse(bufReader, {
     header: true,
     comment: "#",
   });
-
   Deno.close(file.rid);
+
   console.log(result);
 }
 
