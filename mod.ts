c917e0d0d0c17f0b9b0505d58b547c67da65d55f
Anthony Curtis <tonydc1997@gmail.com>
2020-07-06 12:09:57 -0400

    chore: Import modules to parse info at smaller chunks

--- mod.ts
+++ mod.ts
@@ -1,4 +1,6 @@
 import { join } from "https://deno.land/std/path/mod.ts";
+import { BufReader } from "https://deno.land/std/io/bufio.ts";
+import { parse } from "https://deno.land/std/encoding/csv.ts";
 
 async function loadPlanetsData() {
   const path = join(".", "kepler_exoplanets_nasa.csv");
