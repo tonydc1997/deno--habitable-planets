8ba6b8a56a20044c927e6fe449194c358c5171bb
Anthony Curtis <tonydc1997@gmail.com>
2020-07-06 12:01:21 -0400

    feat: Create loadPlanets fn()
    - Resolve/join paths on either OSX, Linux, or Windows OS's.
    
    - Run function.

--- mod.ts
+++ mod.ts
@@ -1,1 +1,10 @@
 import { join } from "https://deno.land/std/path/mod.ts";
+
+async function loadPlanetsData() {
+  const path = join(".", "kepler_exoplanets_nasa.csv");
+  const data = await Deno.open(path);
+
+  console.log(data);
+}
+
+await loadPlanetsData();
