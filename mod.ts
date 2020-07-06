898ba35a94c85686d4fcb9e6b573295e05e05395
Anthony Curtis <tonydc1997@gmail.com>
2020-07-06 12:43:50 -0400

    feat: Narrow results by finding star system similar to ours

--- mod.ts
+++ mod.ts
@@ -16,12 +16,15 @@ async function loadPlanetsData() {
   const habitablePlanets = result.filter((planet) => {
     const planetaryRadius = planet["koi_prad"];
     const stellarMass = planet["koi_smass"];
+    const stellarRadius = planet["koi_srad"];
     return (
       planet["koi_disposition"] === "CONFIRMED" &&
       planetaryRadius > 0.5 &&
       planetaryRadius <= 1.5 &&
       stellarMass > 0.78 &&
-      stellarMass < 1.04
+      stellarMass < 1.04 &&
+      stellarRadius > 0.99 &&
+      stellarRadius < 1.01
     );
   });
 
