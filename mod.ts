328beb96417778b1e42d26b00e5c8e2d6271a4eb
Anthony Curtis <tonydc1997@gmail.com>
2020-07-06 12:13:53 -0400

    fix/feat: Ensure file is passed to bufReader

--- mod.ts
+++ mod.ts
@@ -5,7 +5,7 @@ import { parse } from "https://deno.land/std/encoding/csv.ts";
 async function loadPlanetsData() {
   const path = join(".", "kepler_exoplanets_nasa.csv");
   const file = await Deno.open(path);
-  const bufReader = new BufReader();
+  const bufReader = new BufReader(file);
   const result = await parse(bufReader, {
     header: true,
     comment: "#",
